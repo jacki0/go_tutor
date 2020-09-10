a20cee864085b34ed1f5b7e778a7a968c7d2c80e
jacki0 <jackio@tuta.io>
2020-09-10 23:25:52 +0300

    Strings

--- golang-book/chapter2/main.go
+++ golang-book/chapter2/main.go
@@ -2,8 +2,8 @@ package main
 
 import "fmt"
 
-// this is a comment
-
 func main() {
-	fmt.Println("Hello World!")
+    fmt.Println(len("Hello World"))
+    fmt.Println("Hello World"[1])
+    fmt.Println("Hello " + "World")
 }
