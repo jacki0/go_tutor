178c410577f0390e6a4d876e5fded24bbd381cf7
jacki0 <jackio@tuta.io>
2020-09-11 22:26:25 +0300

    Switch

--- golang-book/chapter5/main.go
+++ golang-book/chapter5/main.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+
 /*
 func main() {
     fmt.Println(1)
@@ -21,7 +22,7 @@ func main() {
         fmt.Println(i)
         i += 1
     }
-}*/
+}
 
 func main() {
     for i := 1; i <= 10; i++ {
@@ -31,5 +32,25 @@ func main() {
             fmt.Println(i, "odd")
         }
     }
-}
+}*/
 
+func main() {
+	for i := 1; i <= 6; i++ {
+		switch i {
+		case 0:
+			fmt.Println("Zero")
+		case 1:
+			fmt.Println("One")
+		case 2:
+			fmt.Println("Two")
+		case 3:
+			fmt.Println("Three")
+		case 4:
+			fmt.Println("Four")
+		case 5:
+			fmt.Println("Five")
+		default:
+			fmt.Println("Unknown Number")
+		}
+	}
+}
