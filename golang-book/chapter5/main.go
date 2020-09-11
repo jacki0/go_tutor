a27b68432ff90f43fad09508d36d582475d275e6
jacki0 <jackio@tuta.io>
2020-09-11 22:46:53 +0300

    FizzBuzz

--- golang-book/chapter5/main.go
+++ golang-book/chapter5/main.go
@@ -53,11 +53,25 @@ func main() {
 			fmt.Println("Unknown Number")
 		}
 	}
-}*/
+}
 
 func main() {
     for i := 1; i <= 100; i++ {
         if i % 3 == 0 {
+            fmt.Println(i)
+        }
+    }
+}*/
+
+func main() {
+    for i := 1; i <= 100; i++ {
+        if i % 3 == 0 && i % 4 == 0 {
+            fmt.Println("FizzBuzz")
+        } else if i % 3 == 0 {
+            fmt.Println("Fizz")
+        } else if i % 4 == 0 {
+            fmt.Println("Buzz")
+        } else {
             fmt.Println(i)
         }
     }
