3a90aa164373320194df95d4da0b347c6f179f42
jacki0 <jackio@tuta.io>
2020-09-12 22:49:17 +0300

    Maps

--- golang-book/chapter6/main.go
+++ golang-book/chapter6/main.go
@@ -89,7 +89,7 @@ func main() {
     if name, ok := elements["Un"]; ok {
         fmt.Println(name, ok)
     }
-}*/
+}
 
 func main() {
 	elements := map[string]string{
@@ -107,5 +107,54 @@ func main() {
 
 	if name, ok := elements["Un"]; ok {
 		fmt.Println(name, ok)
+	}
+}*/
+
+func main() {
+	elements := map[string]map[string]string{
+		"H": map[string]string{
+			"name":  "Hydrogen",
+			"state": "gas",
+		},
+		"He": map[string]string{
+			"name":  "Helium",
+			"state": "gas",
+		},
+		"Li": map[string]string{
+			"name":  "Lithium",
+			"state": "solid",
+		},
+		"Be": map[string]string{
+			"name":  "Beryllium",
+			"state": "solid",
+		},
+		"B": map[string]string{
+			"name":  "Boron",
+			"state": "solid",
+		},
+		"C": map[string]string{
+			"name":  "Carbon",
+			"state": "solid",
+		},
+		"N": map[string]string{
+			"name":  "Nitrogen",
+			"state": "gas",
+		},
+		"O": map[string]string{
+			"name":  "Oxygen",
+			"state": "gas",
+		},
+		"F": map[string]string{
+			"name":  "Fluorine",
+			"state": "gas",
+		},
+		"Ne": map[string]string{
+			"name":  "Neon",
+			"state": "gas",
+		},
+	}
+
+	if el, ok := elements["F"]; ok {
+		fmt.Println(el["name"], el["state"])
 	}
 }
