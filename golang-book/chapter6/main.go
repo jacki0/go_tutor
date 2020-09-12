61210d4750a346c254208dbc55df2c500c4a5943
jacki0 <jackio@tuta.io>
2020-09-12 22:05:33 +0300

    arrays

--- golang-book/chapter6/main.go
+++ golang-book/chapter6/main.go
@@ -6,7 +6,8 @@ func main() {
     var x [5]int
     x[4] = 100
     fmt.Println(x)
-}*/
+}
+
 func main() {
     var x [5]float64
     x[0] = 98
@@ -18,6 +19,21 @@ func main() {
     var total float64 = 0
     for i := 0; i < len(x); i++ {
         total += x[i]
+    }
+    fmt.Println(total / float64(len(x)))
+}*/
+
+func main() {
+    var x [5]float64
+    x[0] = 98
+    x[1] = 93
+    x[2] = 77
+    x[3] = 82
+    x[4] = 83 
+
+    var total float64 = 0
+    for _, value := range x {
+        total += value
     }
     fmt.Println(total / float64(len(x)))
 }
