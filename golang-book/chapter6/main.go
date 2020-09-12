9794e5be1b590ec8d3bf9e573b071b5fadc25abf
jacki0 <jackio@tuta.io>
2020-09-12 22:20:51 +0300

    Slices.copy

--- golang-book/chapter6/main.go
+++ golang-book/chapter6/main.go
@@ -45,10 +45,17 @@ func main() {
         total += value
     }
     fmt.Println(total / float64(len(x)))
-}*/
+}
 
 func main() {
     slice1 := []int{1, 2, 3}
     slice2 := append(slice1, 4, 5)
+    fmt.Println(slice1, slice2)
+}*/
+
+func main() {
+    slice1 := []int{1, 2, 3}
+    slice2 := make([]int, 2)
+    copy(slice2, slice1)
     fmt.Println(slice1, slice2)
 }
