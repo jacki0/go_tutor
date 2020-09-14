efcd3eb1df7b5b0ee5022375d3fa56259392eefd
jacki0 <jackio@tuta.io>
2020-09-14 09:54:28 +0300

    Functions.Closures

--- golang-book/chapter7/main.go
+++ golang-book/chapter7/main.go
@@ -31,11 +31,20 @@ func main() {
     xs := []int{1, 2, 3}
     fmt.Println(add(xs...))
 }
-*/
 
 func main() {
     add := func(x, y int) int {
         return x + y
     }
     fmt.Println(add(1, 1))
+}*/
+
+func main() {
+    x := 0
+    increment := func() int {
+        x++
+        return x
+    }
+    fmt.Println(increment())
+    fmt.Println(increment())    
 }
