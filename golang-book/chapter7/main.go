a4346a6c80a9f336101893e11e1d54a04f28bb80
jacki0 <jackio@tuta.io>
2020-09-14 09:52:44 +0300

    Functions.Closures

--- golang-book/chapter7/main.go
+++ golang-book/chapter7/main.go
@@ -13,7 +13,7 @@ func average(xs []float64) float64 {
 func main() {
     xs := []float64{98, 93, 77, 82, 83}
     fmt.Println(average(xs))
-}*/
+}
 
 func add(args ...int) int {
     total := 0
@@ -22,11 +22,20 @@ func add(args ...int) int {
     }
     return total
 }
-/*
+
 func main() {
     fmt.Println(add(1, 2, 3))
-}*/
+}
+
 func main() {
     xs := []int{1, 2, 3}
     fmt.Println(add(xs...))
+}
+*/
+
+func main() {
+    add := func(x, y int) int {
+        return x + y
+    }
+    fmt.Println(add(1, 1))
 }
