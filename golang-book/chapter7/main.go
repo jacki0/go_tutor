538e82dddc512a13d6ba66a3018abeda3c1adaf0
jacki0 <jackio@tuta.io>
2020-09-14 09:42:26 +0300

    Functions

--- golang-book/chapter7/main.go
+++ golang-book/chapter7/main.go
@@ -1,7 +1,7 @@
 package main
 
 import "fmt"
-
+/*
 func average(xs []float64) float64 {
     total := 0.0
     for _, v := range xs {
@@ -13,4 +13,16 @@ func average(xs []float64) float64 {
 func main() {
     xs := []float64{98, 93, 77, 82, 83}
     fmt.Println(average(xs))
+}*/
+
+func add(args ...int) int {
+    total := 0
+    for _, v := range args {
+        total += v
+    }
+    return total
+}
+
+func main() {
+    fmt.Println(add(1, 2, 3))
 }
