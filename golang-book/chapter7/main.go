7e2d07eda6b3cf923979351b81d817cc4c74861e
jacki0 <jackio@tuta.io>
2020-09-20 14:09:26 +0300

    recursion

--- golang-book/chapter7/main.go
+++ golang-book/chapter7/main.go
@@ -1,7 +1,7 @@
 package main
 
-import "fmt"
-/*
+/*import "fmt"
+
 func average(xs []float64) float64 {
     total := 0.0
     for _, v := range xs {
@@ -47,7 +47,7 @@ func main() {
     }
     fmt.Println(increment())
     fmt.Println(increment())
-}*/
+}
 
 func maleEvenGenerator() func() uint {
     i :=  uint(0)
@@ -63,4 +63,16 @@ func main() {
     fmt.Println(nextEven()) // 0
     fmt.Println(nextEven()) // 2
     fmt.Println(nextEven()) // 4
+}
+*/
+
+func factorial(x uint) uint {
+    if x == 0 {
+        return 1
+    }
+    return x * factorial(x - 1)
+}
+
+func main() {
+    factorial(2)
 }
