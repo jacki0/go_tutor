f0bcea37e573fd7931ec692606a669443b4f409f
jacki0 <jackio@tuta.io>
2020-11-14 14:21:58 +0300

    Pointers

--- golang-book/chapter8/main.go
+++ golang-book/chapter8/main.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-func zero(x int) {
-    x = 0
+func zero(xPtr *int) {
+    *xPtr = 0
 }
 func main() {
     x := 5
-    zero(x)
-    fmt.Println(x) // x всё ещё равен 5
+    zero(&x)
+    fmt.Println(x) // x is 0
 }
