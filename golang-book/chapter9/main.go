e791c14bcb2f48511962acf32a3838f8a6d4e28e
jacki0 <jackio@tuta.io>
2020-11-15 03:54:45 +0300

    Structs

--- golang-book/chapter9/main.go
+++ golang-book/chapter9/main.go
@@ -5,6 +5,14 @@ import ("fmt"; "math")
 type Circle struct {
     x, y, r float64
 }
+type Rectangle struct {
+    x1, y1, x2, y2 float64
+}
+func (r *Rectangle) area() float64 {
+    l := distance(r.x1, r.y1, r.x1, r.y2)
+    w := distance(r.x1, r.y1, r.x2, r.y1)
+    return l * w
+}
 func (c *Circle) area() float64 {
     return math.Pi * math.Pow(c.r, 2)
 }
@@ -12,20 +20,19 @@ func distance(x1, y1, x2, y2 float64) float64 {
     a := x2 - y1
     b := y2 - y1
     return math.Sqrt(a * a + b * b)
-}
+}/*
 func rectangleArea(x1, y1, x2, y2 float64) float64 {
     l := distance(x1, y1, x1, y2)
     w := distance(x1, y1, x2, y1)
     return l * w
-}/*
+}
 func circleArea(c *Circle) float64 {
     return math.Pi * math.Pow(c.r, 2)
 }*/
 func main() {
-    var rx1, ry1 float64 = 0, 0
-    var rx2, ry2 float64 = 10, 10
     c := Circle{0, 0, 5}
+    r := Rectangle{0, 0, 10, 10}
 
-    fmt.Println(rectangleArea(rx1, ry1, rx2, ry2))
+    fmt.Println(r.area())
     fmt.Println(c.area())
 }
