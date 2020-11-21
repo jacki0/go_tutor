a3bfc2985881aa409b605dd64bd5719d6a7a885d
jacki0 <jackio@tuta.io>
2020-11-21 22:32:43 +0300

    Concurrency

--- golang-book/chapter10/main.go
+++ golang-book/chapter10/main.go
@@ -7,9 +7,17 @@ func f(n int) {
         fmt.Println(n, ":", i)
     }
 }
-
+/*
 func main() {
     go f(0)
     var input string
     fmt.Scanln(&input)
+}*/
+func main() {
+    for i := 0; i < 10; i ++ {
+        go f(i)
+    }
+    var input string
+    fmt.Scanln(&input)
 }
+
