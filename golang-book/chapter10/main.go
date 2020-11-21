bfe106999a661439073f48eabc03aad7579ea270
jacki0 <jackio@tuta.io>
2020-11-21 22:39:39 +0300

    Concurrency

--- golang-book/chapter10/main.go
+++ golang-book/chapter10/main.go
@@ -1,23 +1,41 @@
 package main
 
-import "fmt"
-
+import (
+    "fmt"
+    "time"
+    "math/rand"
+)
+/*
 func f(n int) {
     for i := 0; i < 10; i ++ {
         fmt.Println(n, ":", i)
     }
 }
-/*
+
 func main() {
     go f(0)
     var input string
     fmt.Scanln(&input)
-}*/
+}
 func main() {
     for i := 0; i < 10; i ++ {
         go f(i)
     }
     var input string
     fmt.Scanln(&input)
-}
+}*/
 
+func f(n int) {
+    for i := 0; i < 10; i ++ {
+        fmt.Println(n, ":", i)
+        amt := time.Duration(rand.Intn(250))
+        time.Sleep(time.Millisecond * amt)
+    }
+}
+func main() {
+    for i := 0; i < 10; i++ {
+        go f(i)
+    }
+    var input string
+    fmt.Scanln(&input)
+}
