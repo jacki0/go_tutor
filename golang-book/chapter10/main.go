b34505f14b814fd798d9f71bed2860ec3eb92877
jacki0 <jackio@tuta.io>
2020-11-21 23:09:15 +0300

    Channels

--- golang-book/chapter10/main.go
+++ golang-book/chapter10/main.go
@@ -45,6 +45,11 @@ func pinger(c chan string) {
         c <- "ping"
     }
 }
+func ponger(c chan string) {
+    for i := 0; ; i++ {
+        c <- "pong"
+     }
+}
 func printer(c chan string) {
     for {
         msg := <- c
@@ -56,6 +61,7 @@ func main() {
     var c chan string = make(chan string)
 
     go pinger(c)
+    go ponger(c)
     go printer(c)
 
     var input string
