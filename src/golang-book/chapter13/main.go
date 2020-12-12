bf0f2d9ce49ea689ca5ca875196cd5c80ca9e15b
jacki0 <jackio@tuta.io>
2020-12-13 01:44:43 +0300

    core packages.strings.io

--- src/golang-book/chapter13/main.go
+++ src/golang-book/chapter13/main.go
@@ -2,29 +2,15 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"io/ioutil"
 )
 
 func main() {
-	file, err := os.Open("test.txt")
+	bs, err := ioutil.ReadFile("test.txt")
 	if err != nil {
 		//  здесь перехватывается ошибка
 		return
 	}
-	defer file.Close()
-
-	// получить размер файла
-	stat, err := file.Stat()
-	if err != nil {
-		return
-	}
-	// чтение файла
-	bs := make([]byte, stat.Size())
-	_, err = file.Read(bs)
-	if err != nil {
-		return
-	}
-
 	str := string(bs)
 	fmt.Println(str)
 }
