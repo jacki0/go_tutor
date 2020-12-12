68473c7c6935bfefef62cf9e919346cc455bc177
jacki0 <jackio@tuta.io>
2020-12-13 00:37:07 +0300

    testing

--- src/golang-book/chapter11/math/math.go
+++ src/golang-book/chapter11/math/math.go
@@ -3,19 +3,21 @@ package math
 // Найти среднее в массиве чисел.
 func Average(xs []float64) float64 {
 	total := float64(0)
-	if len(xs) != 0 {
-		for _, x := range xs {
-			total += x
-		}
-		return total / float64(len(xs))
-	} else {
+	if len(xs) == 0 {
 		return 0
 	}
+	for _, x := range xs {
+		total += x
+	}
+	return total / float64(len(xs))
 }
 
 //Найти меньшее в срезе чисел
 func Min(xs []float64) float64 {
 	min := float64(1000 * 1000)
+	if len(xs) == 0 {
+		return 0
+	}
 	for _, x := range xs {
 		if x < min {
 			min = x
--- src/golang-book/chapter11/math/math_test.go
+++ src/golang-book/chapter11/math/math_test.go
@@ -10,7 +10,7 @@ func TestAverage(t *testing.T) {
 		t.Error("Expected 1.5, got ", v)
 	}
 }
-*/
+
 
 type testpair struct {
 	values  []float64
@@ -31,6 +31,31 @@ func TestAverage(t *testing.T) {
 			t.Error(
 				"For", pair.values,
 				"expected", pair.average,
+				"got", v,
+			)
+		}
+	}
+}*/
+
+type testpair struct {
+	values []float64
+	min    float64
+}
+
+var tests = []testpair{
+	{[]float64{1, 2}, 1},
+	{[]float64{1, 1, 1, 1, 1, 1, 1}, 1},
+	{[]float64{-1, 1}, -1},
+	{[]float64{}, 0},
+}
+
+func TestMin(t *testing.T) {
+	for _, pair := range tests {
+		v := Min(pair.values)
+		if v != pair.min {
+			t.Error(
+				"For", pair.values,
+				"expected", pair.min,
 				"got", v,
 			)
 		}
