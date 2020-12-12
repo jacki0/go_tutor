e8ced9ad4357a37b2793974f0268cf267703fb6f
jacki0 <jackio@tuta.io>
2020-12-12 23:57:48 +0300

    packages

--- src/golang-book/chapter11/main.go
+++ src/golang-book/chapter11/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
-import m "golang-book/chapter11/math"
+import (
+	"fmt"
+	m "golang-book/chapter11/math"
+)
 
 func main() {
-    xs := []float64{1,2,3,4}
-    avg := m.Average(xs)
-    fmt.Println(avg)
+	xs := []float64{1, 2, 3, 4}
+	avg := m.Average(xs)
+	minimal := m.Min(xs)
+	fmt.Println(avg)
+	fmt.Println(minimal)
 }
--- src/golang-book/chapter11/math/math.go
+++ src/golang-book/chapter11/math/math.go
@@ -8,3 +8,14 @@ func Average(xs []float64) float64 {
 	}
 	return total / float64(len(xs))
 }
+
+//Найти меньшее в срезе чисел
+func Min(xs []float64) float64 {
+	min := float64(1000 * 1000)
+	for _, x := range xs {
+		if x < min {
+			min = x
+		}
+	}
+	return min
+}
