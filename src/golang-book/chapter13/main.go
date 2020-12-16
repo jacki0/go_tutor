5f70dedf0c72fcdd6d9ee57516a7ebf93c6b7d26
jacki0 <jackio@tuta.io>
2020-12-16 15:23:37 +0300

    container/sort

--- src/golang-book/chapter13/main.go
+++ src/golang-book/chapter13/main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
-	"container/list"
 	"fmt"
+	"sort"
 )
 
-func main() {
-	var x list.List
-	x.PushBack(1)
-	x.PushBack(2)
-	x.PushBack(3)
+type Person struct {
+	Name string
+	Age  int
+}
 
-	for e := x.Front(); e != nil; e = e.Next() {
-		fmt.Println(e.Value.(int))
+type ByName []Person
+
+func (this ByName) Len() int {
+	return len(this)
+}
+func (this ByName) Less(i, j int) bool {
+	return this[i].Name < this[j].Name
+}
+func (this ByName) Swap(i, j int) {
+	this[i], this[j] = this[j], this[i]
+}
+
+func main() {
+	kids := []Person{
+		{"Jill", 9},
+		{"Jack", 10},
 	}
+	sort.Sort(ByName(kids))
+	fmt.Println(kids)
 }
