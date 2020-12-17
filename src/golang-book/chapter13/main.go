6c79be70dd3c3c2b1406196383cb02b201f9c377
jacki0 <jackio@tuta.io>
2020-12-17 21:26:59 +0300

    http server

--- src/golang-book/chapter13/main.go
+++ src/golang-book/chapter13/main.go
@@ -1,66 +1,30 @@
 package main
 
 import (
-	"encoding/gob"
-	"fmt"
-	"net"
+	"io"
+	"net/http"
 )
 
-func server() {
-	// слушать порт
-	ln, err := net.Listen("tcp", ":9999")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	for {
-		// принятие соединения
-		c, err := ln.Accept()
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		// обработка соединения
-		go handleServerConnection(c)
-	}
-}
-
-func handleServerConnection(c net.Conn) {
-	// получение сообщения
-	var msg string
-	err := gob.NewDecoder(c).Decode(&msg)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("Received", msg)
-	}
-
-	c.Close()
-}
-
-func client() {
-	// соединиться с сервером
-	c, err := net.Dial("tcp", "127.0.0.1:9999")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// послать сообщение
-	msg := "Hello World"
-	fmt.Println("Sending", msg)
-	err = gob.NewEncoder(c).Encode(msg)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	c.Close()
+func hello(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set(
+		"Content-Type",
+		"text/html",
+	)
+	io.WriteString(
+		res,
+		`<doctype html>
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body>
+        Hello World!
+    </body>
+</html>`,
+	)
 }
 
 func main() {
-	go server()
-	go client()
-
-	var input string
-	fmt.Scanln(&input)
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":9000", nil)
 }
