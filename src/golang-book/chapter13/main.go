20444da59a6a4407396ca7f929da20500abb1016
jacki0 <jackio@tuta.io>
2020-12-17 21:20:54 +0300

    encoding/gob & tcp server

--- src/golang-book/chapter13/main.go
+++ src/golang-book/chapter13/main.go
@@ -1,13 +1,66 @@
 package main
 
 import (
-	"crypto/sha1"
+	"encoding/gob"
 	"fmt"
+	"net"
 )
 
+func server() {
+	// слушать порт
+	ln, err := net.Listen("tcp", ":9999")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for {
+		// принятие соединения
+		c, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		// обработка соединения
+		go handleServerConnection(c)
+	}
+}
+
+func handleServerConnection(c net.Conn) {
+	// получение сообщения
+	var msg string
+	err := gob.NewDecoder(c).Decode(&msg)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Received", msg)
+	}
+
+	c.Close()
+}
+
+func client() {
+	// соединиться с сервером
+	c, err := net.Dial("tcp", "127.0.0.1:9999")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// послать сообщение
+	msg := "Hello World"
+	fmt.Println("Sending", msg)
+	err = gob.NewEncoder(c).Encode(msg)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	c.Close()
+}
+
 func main() {
-	h := sha1.New()
-	h.Write([]byte("test"))
-	bs := h.Sum([]byte{})
-	fmt.Println(bs)
+	go server()
+	go client()
+
+	var input string
+	fmt.Scanln(&input)
 }
